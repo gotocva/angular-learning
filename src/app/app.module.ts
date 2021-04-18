b926d3eb88337ceaac71ddc45892bcfaf07319ec
Sivabharathy <gotocva@gmail.com>
2021-04-18 19:09:12 +0530

    prime ng added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,6 +9,13 @@ import { HeaderComponent } from './header/header.component';
 
 import { FormsModule } from '@angular/forms';
 
+import {CardModule} from 'primeng/card';
+import {MegaMenuModule} from 'primeng/megamenu';
+
+import {MenubarModule} from 'primeng/menubar';
+
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,8 +23,13 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    MegaMenuModule,
+    CardModule,
+    HttpClientModule,
+    MenubarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
